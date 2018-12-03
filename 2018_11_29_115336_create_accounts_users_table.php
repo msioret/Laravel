f2ba05bdc38923d2403ef59ae01d8fa328b5f45a
Mory <44232141+msioret@users.noreply.github.com>
2018-12-03 14:35:10 -0200

    Create 2018_11_29_115336_create_accounts_users_table.php
    aqui esta la migracion :2018_11_29_115336_create_accounts_users_table.php que corresponde a la tabla pivote donde se guardan solo los id de usuarios y los id de adm_account ya que es una relacion de muchos a muchos!

---
+++ 2018_11_29_115336_create_accounts_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts_users', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->integer('account_id')->unsigned();
+            $table->primary(['user_id','account_id']);
+            $table->timestamps();
+            //Relations
+            $table->foreign('user_id')->references('id')->on('SecUsers')->onDelete('cascade');
+            $table->foreign('account_id')->references('id')->on('admAccounts')->onDelete('cascade');
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts_users');
+    }
+}
